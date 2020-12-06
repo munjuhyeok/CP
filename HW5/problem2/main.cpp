e13f9c15f48efffb0f716ab17a8d3132d34ab323
munjuhyeok <munjuhyeok@gmail.com>
2020-12-06 12:30:15 +0900

    HW5 done

--- HW5/problem2/main.cpp
+++ HW5/problem2/main.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <algorithm>
 #include <cmath>
+#include <numeric>
 
 /* =======START OF PRIME-RELATED HELPERS======= */
 /*
@@ -156,55 +157,142 @@ std::pair<int, int> number_vs_number(int a, int b) {
 std::pair<std::multiset<int>, std::multiset<int>> player_battle(
     std::string type_a, std::multiset<int> a, std::string type_b, std::multiset<int> b
 ) {
+    // TODO 2-3
     int n = a.size();
     int m = b.size();
-    std::pair<int,int> loss[n][m];
-    std::multiset<int>::iterator it_a = a.begin();
+
+    //numbers A and B have
+    std::vector<int> a_vector(a.begin(), a.end());
+    std::vector<int> b_vector(b.begin(), b.end());
+
+    std::vector<int> max_loss_a(n,0);
+    std::vector<int> min_loss_a(n,PRIME_TEST_LIMIT);
+    std::vector<int> max_loss_b(m,0);
+    std::vector<int> min_loss_b(m,PRIME_TEST_LIMIT);
 
     for(int i = 0; i<n; i++){
-        std::multiset<int>::iterator it_b = b.begin();
         for(int j = 0; j<m; j++){
-            std::pair<int,int> outcome = number_vs_number(*it_a,*it_b);
-            loss[i][j] = std::make_pair(*it_a - outcome.first, *it_b - outcome.second);
-            it_b++;
+            std::pair<int,int> outcome = number_vs_number(a_vector[i],b_vector[j]);
+            int loss_a = a_vector[i] - outcome.first;
+            int loss_b = b_vector[j] - outcome.second;
+            max_loss_a[i] = std::max(max_loss_a[i],loss_a);
+            min_loss_a[i] = std::min(min_loss_a[i],loss_a);
+            max_loss_b[j] = std::max(max_loss_b[j],loss_b);
+            min_loss_b[j] = std::min(min_loss_b[j],loss_b);
         }
-        it_a++;
     }
 
+    std::vector<int> max_regret_a(n,-PRIME_TEST_LIMIT);
+    std::vector<int> max_regret_b(m,-PRIME_TEST_LIMIT);
+    for(int i = 0; i<n; i++){
+        std::vector<int> min_loss_except_ith_element = min_loss_a;
+        min_loss_except_ith_element.erase(min_loss_except_ith_element.begin()+i);
+        max_regret_a[i] = max_loss_a[i] - *std::min_element(min_loss_except_ith_element.begin(), min_loss_except_ith_element.end());
+    }
+    for(int i = 0; i<m; i++){
+        std::vector<int> min_loss_except_ith_element = min_loss_b;
+        min_loss_except_ith_element.erase(min_loss_except_ith_element.begin()+i);
+        max_regret_b[i] = max_loss_b[i] - *std::min_element(min_loss_except_ith_element.begin(), min_loss_except_ith_element.end());
+    }
+    // which number to choose from a_vector, b_vector
+    int index_a, index_b;
+
     if(type_a == MAXIMIZE_GAIN) {
-        loss
+        index_a = std::min_element(min_loss_a.begin(),min_loss_a.end()) - min_loss_a.begin();
     }else if(type_a == MINIMIZE_LOSS){
-
+        index_a = std::min_element(max_loss_a.begin(),max_loss_a.end()) - max_loss_a.begin();
     }else if(type_a == MINIMIZE_REGRET) {
-
+        index_a = std::min_element(max_regret_a.begin(),max_regret_a.end()) - max_regret_a.begin();
     }
 
     if(type_b == MAXIMIZE_GAIN) {
-
+        index_b = std::min_element(min_loss_b.begin(),min_loss_b.end()) - min_loss_b.begin();
     }else if(type_b == MINIMIZE_LOSS){
-
+        index_b = std::min_element(max_loss_b.begin(),max_loss_b.end()) - max_loss_b.begin();
     }else if(type_b == MINIMIZE_REGRET) {
-
+        index_b = std::min_element(max_regret_b.begin(),max_regret_b.end()) - max_regret_b.begin();
     }
-    // TODO 2-3
-    return std::pair<std::multiset<int>, std::multiset<int>>();
+
+    int num_a = a_vector[index_a];
+    int num_b = b_vector[index_b];
+
+    std::pair<int,int> outcome = number_vs_number(num_a, num_b);
+
+    num_a = outcome.first;
+    num_b = outcome.second;
+
+    a_vector[index_a] = num_a;
+    b_vector[index_b] = num_b;
+
+    a = std::multiset<int>(a_vector.begin(), a_vector.end());
+    b = std::multiset<int>(b_vector.begin(), b_vector.end());
+
+    return std::pair<std::multiset<int>, std::multiset<int>>(a,b);
 }
 
 std::pair<std::multiset<int>, std::multiset<int>> player_vs_player(
     std::string type_a, std::multiset<int> a, std::string type_b, std::multiset<int> b
 ) {
     // TODO 2-4
-    return std::pair<std::multiset<int>, std::multiset<int>>();
+    while (true){
+        std::pair<std::multiset<int>, std::multiset<int>> outcome = player_battle(type_a,a,type_b,b);
+        std::multiset<int> new_a = outcome.first;
+        std::multiset<int> new_b = outcome.second;
+        if(a == new_a && b == new_b){
+            break;
+        }
+        a = new_a;
+        b = new_b;
+    }
+    return std::pair<std::multiset<int>, std::multiset<int>>(a,b);
 }
 
 int tournament(std::vector<std::pair<std::string, std::multiset<int>>> players) {
     // TODO 2-5
-    return -1;
+    std::vector<int> survivors(players.size()); //ID of survivors, 0~players.size()
+    std::iota(survivors.begin(), survivors.end(), 0);
+
+    while(survivors.size() != 1) {
+        int num_match = survivors.size() / 2;
+        std::vector<int> losers;
+        for (int i = 0; i < num_match; i++) {
+            int id_a = survivors[2 * i];
+            int id_b = survivors[2 * i + 1];
+
+            std::string type_a = players[id_a].first;
+            std::string type_b = players[id_b].first;
+            std::multiset<int> a = players[id_a].second;
+            std::multiset<int> b = players[id_b].second;
+            std::pair<std::multiset<int>, std::multiset<int>> outcome = player_vs_player(type_a, a, type_b, b);
+
+            std::multiset<int> new_a = outcome.first;
+            std::multiset<int> new_b = outcome.second;
+
+            int sum_a = std::accumulate(new_a.begin(), new_a.end(), 0);
+            int sum_b = std::accumulate(new_b.begin(), new_b.end(), 0);
+
+            if(sum_a<sum_b){
+                losers.push_back(id_a);
+            } else{
+                losers.push_back(id_b);
+            }
+        }
+        for(int id :losers){
+            survivors.erase(std::remove(survivors.begin(), survivors.end(), id), survivors.end());
+        }
+    }
+    return survivors[0];
 }
 
 int steady_winner(std::vector<std::pair<std::string, std::multiset<int>>> players) {
     // TODO 2-6
-    return -1;
+    std::vector<int> num_win(players.size()); //ID of survivors, 0~players.size()
+    for(int i = 0; i<players.size(); i++){
+        int winner = tournament(players);
+        num_win[(winner+i)%players.size()]++;
+        std::rotate(players.begin(),players.begin()+1,players.end());
+    }
+    return std::max_element(num_win.begin(), num_win.end()) - num_win.begin();
 }
 
 /* =======END OF TODOs======= */
@@ -232,13 +320,16 @@ void print_multiset(const std::multiset<int>& m) {
 }
 
 int main() {
-    int question_number; std::cin >> question_number;
+    int question_number;
+    std::cin >> question_number;
     if (question_number == 1) {
-        int a, b; std::cin >> a >> b;
+        int a, b;
+        std::cin >> a >> b;
         std::tie(a, b) = number_fight(a, b);
         std::cout << a << " " << b << std::endl;
     } else if (question_number == 2) {
-        int a, b; std::cin >> a >> b;
+        int a, b;
+        std::cin >> a >> b;
         std::tie(a, b) = number_vs_number(a, b);
         std::cout << a << " " << b << std::endl;
     } else if (question_number == 3 || question_number == 4) {
@@ -247,19 +338,20 @@ int main() {
         std::multiset<int> a_, b_;
         if (question_number == 3) {
             tie(a_, b_) = player_battle(
-                a.first, a.second, b.first, b.second
+                    a.first, a.second, b.first, b.second
             );
         } else {
             tie(a_, b_) = player_vs_player(
-                a.first, a.second, b.first, b.second
+                    a.first, a.second, b.first, b.second
             );
         }
         print_multiset(a_);
         print_multiset(b_);
     } else if (question_number == 5 || question_number == 6) {
-        int num_players; std::cin >> num_players;
+        int num_players;
+        std::cin >> num_players;
         std::vector<player> players;
-        for(int i=0;i<num_players;i++) {
+        for (int i = 0; i < num_players; i++) {
             players.push_back(scan_player());
         }
         int winner_id;
@@ -270,6 +362,6 @@ int main() {
         }
         std::cout << winner_id << std::endl;
     }
-    return 0;
+return 0;
 }
 /* =======END OF MAIN CODE======= */
